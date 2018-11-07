88afa72ff85e449d0f90506ee8ea6a4e6d3225d5
pedro <pnevadom42@elpuig.xeill.net>
2018-11-07 19:59:01 +0100

    basic2

--- src/com/company/Basic2.java
+++ src/com/company/Basic2.java
@@ -12,6 +12,7 @@ public class Basic2 {
         System.out.println("Introduce el numero 1 si deseas multiplicar  // Introduce el numero 2 si deseas sumarlos");
         int introduce = in.nextInt();
 
+
         int resultado =0;
 
         if(introduce == 2){
